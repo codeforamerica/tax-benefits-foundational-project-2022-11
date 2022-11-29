9312879d6ec7f8116ebcd7725519fafffdc598e1
Lauren Kemperman <lkemperman@codeforamerica.org>
2022-11-29 15:24:51 -0500

    add datatable, emoji clipboard, and placeholder link on new application button
    Co-authored-by: Jacky Alcine <jalcine@codeforamerica.org>

--- spec/views/benefits_applications/index.html.erb_spec.rb
+++ spec/views/benefits_applications/index.html.erb_spec.rb
@@ -6,10 +6,17 @@ RSpec.describe "benefits_applications/index.html.erb", type: :view do
     render
     expect(rendered).to match(/Submitted Applications/)
   end
+
   it "shows a way to add a new application" do
     render
     expect(rendered).to match(/New Application/)
-
   end
-  # test "that the benefits application table view has the expected columns"
+
+  it "that the benefits application table view has the expected columns" do
+    render
+
+    expect(rendered).to match(/App number/)
+    expect(rendered).to match(/Date submitted/)
+    expect(rendered).to match(/Applicant/)
+  end
 end
