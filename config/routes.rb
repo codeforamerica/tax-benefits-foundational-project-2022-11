9196de08956b15b9e9be54a9db189271b13fd3f9
Jacky Alcine <jalcine@codeforamerica.org>
2022-12-23 11:38:20 -0500

    Remove dupe routes
    Co-authored-by: Lauren Kemperman <lkemperman@codeforamerica.org>

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   get 'new' => 'benefits_applications#new', as: :new_benefit_app
   post 'create' => 'benefits_applications#create', as: :benefit_apps
-  delete 'delete_benefit_app/:benefit_app_id' => 'benefits_applications#delete_benefit_app', as: :delete_benefit_app
+  delete 'delete_benefit_app/:benefit_app_id' => 'benefits_applications#delete_benefit_app',
+         as: :delete_benefit_app
   get 'new_member' => 'benefits_applications#new_member', as: :new_member
   post 'create_member' => 'benefits_applications#create_member', as: :members
   get 'validate_application' => 'benefits_applications#validate_application'
 
-  patch 'update_member/:id/update', to: 'benefits_applications#update_member', as: 'update_member'
-  get 'members/:id/edit', to: 'benefits_applications#edit_member', as: 'edit_member'
-  get 'delete_member/:member_id' => 'benefits_applications#delete_member', as: :delete_member
-
   patch 'update_member/:id', to: 'benefits_applications#update_member', as: :member
   get 'members/:id', to: 'benefits_applications#edit_member', as: 'edit_member'
-  get 'benefits_apps/:benefit_app_id', to: 'benefits_applications#edit_benefits_app', as: :edit_benefits_app
+  get 'benefits_apps/:benefit_app_id', to: 'benefits_applications#edit_benefits_app',
+                                       as: :edit_benefits_app
   get 'delete_member/:member_id' => 'benefits_applications#delete_member', as: :delete_member
-  patch 'update_benefits_apps/:benefit_app_id', to: 'benefits_applications#update_benefits_app', as: :update_benefits_app
-  patch 'update_benefits_apps/:benefit_app_id', to: 'benefits_applications#update_benefits_app', as: :benefit_app
+  patch 'update_benefits_apps/:benefit_app_id', to: 'benefits_applications#update_benefits_app',
+                                                as: :update_benefits_app
+  patch 'update_benefits_apps/:benefit_app_id', to: 'benefits_applications#update_benefits_app',
+                                                as: :benefit_app
   root 'benefits_applications#index'
 end
